dfe12d118e8e9d40c512790cd8f8659edc3cc139
ttshrk@gmail.com <ttshrk@gmail.com@e042b6aa-1ee6-4664-cef0-8ecfcf6c78d7>
2011-09-12 07:42:03 +0000

    support android library

--- trunk/src/com/ttshrk/view/DateScrollPickerView.java
+++ trunk/src/com/ttshrk/view/DateScrollPickerView.java
@@ -28,26 +28,14 @@ public class DateScrollPickerView extends ScrollPickerView {
 	}
 	
 	protected void init() {
-
-		addSlot(new String[] { 
-				"1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979", 
-				"1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", 
-				"1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", 
-				"2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", 
-				"2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019",
-				}, 5, ScrollType.Ranged);
-		addSlot(new String[] { "年" }, 2, ScrollType.None);
-		addSlot(new String[] { 
-				"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
-				}, 5, ScrollType.Ranged);
-		addSlot(new String[] { "月" }, 2, ScrollType.None);
-		addSlot(new String[] { 
-				"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-				"11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
-				"21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
-				"31",
-				}, 5, ScrollType.Loop);
-		addSlot(new String[] { "日" }, 2, ScrollType.None);
+		String[] separater = getResources().getStringArray(com.ttshrk.view.R.array.com_ttshrk_view_scroll_picker_ymd_separater);
+		addSlot(getResources().getStringArray(com.ttshrk.view.R.array.com_ttshrk_view_scroll_picker_year_list), 5, ScrollType.Ranged);
+		addSlot(new String[]{separater[0]}, 2, ScrollType.None);
+		addSlot(getResources().getStringArray(com.ttshrk.view.R.array.com_ttshrk_view_scroll_picker_month_list), 5, ScrollType.Ranged);
+		addSlot(new String[]{separater[1]}, 2, ScrollType.None);
+		addSlot(getResources().getStringArray(com.ttshrk.view.R.array.scroll_picker_day_list), 5, ScrollType.Loop);
+		if(separater.length > 2)
+			addSlot(new String[]{separater[2]}, 2, ScrollType.None);
 		yearIndex = 0;
 		monthIndex = 2;
 		dayIndex = 4;
